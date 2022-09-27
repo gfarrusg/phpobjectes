108313a2873a6f11eb5eab85727b666b2707fcc9
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-09-27 10:11:03 +0200

    fixing why the last two echoes don't work.
    fixing why the last two echoes don't work.

--- php_objectes.php
+++ php_objectes.php
@@ -2,7 +2,7 @@
 
 echo " <h1>Exercici 1 Tema PHP i Objectes.</h1>";
 
-    // creacio de la class
+    // creacio de la class.
 
     class Employee{
 
@@ -12,28 +12,39 @@ echo " <h1>Exercici 1 Tema PHP i Objectes.</h1>";
 
         public $sou = 7000;
 
-        // Primer metode creat, nomes es el nom i el sou
+        // Primer metode creat, nomes es el nom i el sou.
 
-        public function initialize(){
+            public function initialize($nom, $sou){
 
-            return $this-> nom;
+                return $this-> $nom;
             
-            return $this->sou;
+                return $this-> $sou;
         
-        }
+            }
 
         // segon metode creat per saber si la persona ha de pagar impostos o no.
 
-        public function print(){
+            public function print($sou){
 
+                if($sou <= 6000) {
+    
+                    return "No pagues impostos ja que el sou es inferior a 6000€ ".$sou;
+                }Else{
+                    return "Pagues impostos ja que el sou es superior a 6000€: ".$sou;
+                }
 
-
-
-        }
+               
+            }
 
     }
 
+            //declaracio dels objectes.
 
+    $exercici = new Employee();
+
+    echo $exercici->initialize($nom, $sou);
+
+    echo $exercici->print($sou);
 
 
 ?>
