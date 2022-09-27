4132b93607253aabbe4dddd470bab264d9f59855
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-09-27 09:34:46 +0200

    Add files via upload
    Create de method initialaze and method print

--- php_objectes.php
+++ php_objectes.php
@@ -2,16 +2,33 @@
 
 echo " <h1>Exercici 1 Tema PHP i Objectes.</h1>";
 
+    // creacio de la class
+
     class Employee{
+
+        // Els atributs nom i sou per l'us tan a la clases com en els metodes.
 
         public $nom = "Gabriel Farrus";
 
         public $sou = 7000;
 
+        // Primer metode creat, nomes es el nom i el sou
+
         public function initialize(){
 
             return $this-> nom;
+            
+            return $this->sou;
         
+        }
+
+        // segon metode creat per saber si la persona ha de pagar impostos o no.
+
+        public function print(){
+
+
+
+
         }
 
     }
