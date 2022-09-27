64e4d6b511960edd9f8b747e146496c1252e94ce
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-09-27 09:29:03 +0200

    Add files via upload

---
+++ php_objectes.php
@@ -0,0 +1,22 @@
+<?php
+
+echo " <h1>Exercici 1 Tema PHP i Objectes.</h1>";
+
+    class Employee{
+
+        public $nom = "Gabriel Farrus";
+
+        public $sou = 7000;
+
+        public function initialize(){
+
+            return $this-> nom;
+        
+        }
+
+    }
+
+
+
+
+?>
