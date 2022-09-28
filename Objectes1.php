e67f2b9c465266fd80e7cebf0aaff73c9881bae6
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-09-28 10:00:37 +0200

    I think that is finish

--- Objectes1.php
+++ Objectes1.php
@@ -9,37 +9,45 @@ echo "<h1> Exercici 1 Tema PHP_Objectes.</h1> ";
 
 Class Employee{
 
-    protected $nom;
+    public $nom;
     
-    protected $sou;
+    public $sou;
 
     public function nom ($nom){
 
-        $this -> nom = $nom = "Gabriel Farrus"; 
+        $this -> nom = $nom; 
     }
 
     public function sou ($sou){
        
-        $this -> sou = $sou = 7000;
+        $this -> sou = $sou;
     }
 
-}
 
-         function initialize ($nom, $sou){
 
+        function initialize ($nom, $sou){
+
+            $this -> nom = $nom = "Gabriel Farrus"; 
+
+            $this -> sou = $sou = 7000;
         }
 
         function print ($nom, $sou){
 
             
             if($sou <= 6000){
-                echo .$nom."No pagues impostos, ja que el teu sou no supera els 6000€".$sou.'<br />';
+                echo $nom." No pagues impostos, ja que el teu sou, no supera els 6000€ ".$sou.'<br />';
             } else {
-                echo .$nom."Pagues impostos, ja que els teus ou supera els 6000€".$sou.'<br />';
+                echo $nom." Pagues impostos, ja que el teu sou,  supera els 6000€ ".$sou.'<br />';
             }   
         }
 
+    }
 
+            //declaracio dels objectes.
 
+        $exercici = new Employee();
+
+         echo $exercici->print($nom = "Gabriel Farrus", $sou = 7000);
 
 ?>
