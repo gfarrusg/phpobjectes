2b83f88b3485d2fbbd5a37fb2f7fbcf6bfc684d6
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-10-17 09:13:33 +0200

    corrected

--- SubClassTriangle.php
+++ SubClassTriangle.php
@@ -5,17 +5,17 @@ require_once ("ClassShape.php");
 
 class Triangle extends Shape{
 
-    protected $Areatriangle;
+    protected $areaTriangle;
 
     function __construct (int $ample, int $alt){
 
         parent::__construct($ample,$alt);
 
     }
-    public function triangle($ample, $alt){
+    public function calcularArea($ample, $alt){
         $areatriangle = ($ample * $alt) / 2;
-        echo "Un triangle de base $ample i alçada $alt té un àrea de .";
-        return $areatriangle;
+        echo "Un triangle de base $ample i alçada $alt té un àrea de.";
+        return  $areatriangle ;
         }
 
     
