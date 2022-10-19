b870ba080daeed6cf3e74e75481edca20f6a326a
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-10-19 15:48:58 +0200

    corrected

--- Main.php
+++ Main.php
@@ -4,16 +4,14 @@ require_once("SubClassTriangle.php");
 
     $objTriangle = new Triangle (2, 4);
 
-    echo $objTriangle-> getdades();
-
-    echo $objTriangle-> triangle (2, 4);
+    echo $objTriangle-> calcularArea (2, 4);
 
 require_once("SubClassRectangle.php");
 
     $objRectangle = new Rectangle ( 4, 8);
 
-    echo $objRectangle-> getdades();
-    echo $objRectangle-> rectangle(4,8);
+    
+    echo $objRectangle-> calcularArea (4,8);
 
     
 
