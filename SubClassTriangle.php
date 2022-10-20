4ec19591d25d47bda4c426c6fc704ef9bc1e5945
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-10-20 16:55:50 +0200

    modified and corrected
    the same as in the rectangle subclass

--- SubClassTriangle.php
+++ SubClassTriangle.php
@@ -5,13 +5,13 @@ require_once ("ClassShape.php");
 
 class Triangle extends Shape{
 
-    protected $areaTriangle;
+    
 
     
     public function calcularArea(){
-        $areatriangle = ($this-> ample * $this -> alt) / 2;
+        $area = ($this-> ample * $this -> alt) / 2;
         echo "El triangle te una area de: ";
-        return  $areatriangle.'<br />' ;
+        return  $area.'<br />' ;
     }
 
 }
